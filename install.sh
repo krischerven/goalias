54f9627cd63c40ee73ccfeb2146a002ed1434066
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-07 01:07:04 -0400

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+(rm bin/goalias) > /dev/null 2>&1
+./vet.sh
+./builder.sh
+if [ -f bin/goalias ]; then
+	(sudo rm /usr/local/bin/goalias) > /dev/null 2>&1
+	sudo ln -s "$PWD/bin/goalias" /usr/local/bin/goalias
+	echo "Successfully installed goalias."
+else
+	echo "Error installing goalias." | perl -wln -M'Term::ANSIColor' -e 'print "\e[1;91m", "$_", "\e[0m"'
+fi
