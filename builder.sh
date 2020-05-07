b479704e4a80f8b0c338fb524836aec22f2642f3
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-07 00:11:06 -0400

    builder.sh: update for src/main.go, fix perms error

--- builder.sh
+++ builder.sh
@@ -1,9 +1,10 @@
 mkdir -p /tmp/goalias
+sudo chmod -R 0777 /tmp/goalias
 sudo cp -r "$PWD/." /tmp/goalias
 odir="$PWD"
-cd /tmp/goalias
+cd /tmp/goalias/src
 go build
 env GOOS=windows GOARCH=amd64 go build
 cd "$odir"
-cp /tmp/goalias/goalias "$PWD/bin"
-cp /tmp/goalias/goalias.exe "$PWD/bin"
+cp /tmp/goalias/src/src "$PWD/bin/goalias"
+cp /tmp/goalias/src/src.exe "$PWD/bin/goalias.exe"
