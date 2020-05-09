4c7f047ce4a256163fa690fc41e14ba364072fa3
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-09 16:52:20 -0400

    clean.sh: add a warning when the registry is not empty

--- clean.sh
+++ clean.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ -f "/etc/goalias/registry.txt" ] && [ $(wc -l < /etc/goalias/registry.txt) != 0 ]; then
+	read -p "Registry is not empty; continue? (Y/N)" -n 1 -r
+	echo
+	if ! [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		exit 0
+	fi
+fi
+
 sudo rm -rf /tmp/goalias
 sudo rm -rf /etc/goalias
+echo "cleaned"
