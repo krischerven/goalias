a89a87639d051d1c3cd030d696188941d6a19524
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-09 17:49:33 -0400

    clean.sh: add documentation

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This file is designed for debugging:
+# only use it if you know what you're doing and you don't mind losing all of your aliases
 if [ -f "/etc/goalias/registry.txt" ] && [ $(wc -l < /etc/goalias/registry.txt) != 0 ]; then
 	read -p "Registry is not empty; continue? (Y/N)" -n 1 -r
 	echo
